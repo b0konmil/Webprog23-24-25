96eedf361f1133e715ea4155f84e00efec4f73c0
2020b-Kónya Milán <74898838+b0konmil@users.noreply.github.com>
2024-02-09 09:36:45 +0100

    Add files via upload

---
+++ All or nothing, mean vs median, swap head and tail/script.js
@@ -0,0 +1,79 @@
+// All or Nothing
+function possiblyPerfect(key,answers) {
+    for(let i = 0; i < key.length; i++)
+    {
+        if(answers[i] == "-")
+        {
+            if(key[i] != answers[i])
+            {
+                return false;
+            }
+        }
+        else(answers[i] == key[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+console.log(possiblyPerfect([..."A_C_B"],[..."ADCEB"]) + " >> true ");
+console.log( possiblyPerfect([..."B_B"],[..."BDC"]) + " >> false ");
+console.log( possiblyPerfect([..."T_FFF"],[..."FFTTT"]) + " >> true ");
+console.log( possiblyPerfect([..."BA__"],[..."BACC"]) + " >> true ");
+console.log( possiblyPerfect([..."ABA_"],[..."BACC"]) + " >> true ");
+console.log( possiblyPerfect([..."ABC_"],[..."BACC"]) + " >> false ");
+console.log( possiblyPerfect([..."B_"],[..."CA"]) + " >> true ");
+console.log( possiblyPerfect([..."BA"],[..."CA"]) + " >> false ");
+console.log( possiblyPerfect([..."B"],[..."B"]) + " >> true ");
+console.log( possiblyPerfect([..."_T__"],[..."TFFF"]) + " >> true ");
+console.log( possiblyPerfect([..."_T__"],[..."TTFT"]) + " >> true ");
+console.log( possiblyPerfect([..."_TTT"],[..."TTFT"]) + " >> false ");
+console.log( possiblyPerfect([..."_TTT"],[..."TTTT"]) + " >> true ");
+console.log( possiblyPerfect([..."_TTT"],[..."FFFF"]) + " >> true ");
+console.log( possiblyPerfect([..."____"],[..."FFFF"]) + " >> true ");
+
+
+
+// Mean vs. Median
+function meanVsMedian(numbers) {
+    let sum = 0;
+    let median = numbers.length/2;
+    let mean = sum/numbers.length;
+    for(let i = 0; i < numbers.length; i++)
+    {
+        sum = numbers[i];
+    }
+    if(mean > median)
+    {
+        console.log("mean");
+    }
+    else if(mean < median)
+    {
+        console.log("median");
+    }
+    else
+    {
+        console.log("same");
+    }
+    
+}
+
+console.log(meanVsMedian([1, 1, 1]), ' >> same');
+console.log(meanVsMedian([1, 2, 37]), ' >> mean');
+console.log(meanVsMedian([7, 14, -70]), ' >> median');
+
+
+
+// Swap the head and the tail
+function swapHeadAndTail(arr) {
+    let middle = arr.length/2;
+    let head = arr.slice(0, middle);
+    let tail = arr.slice(middle);
+    let swapped = tail.concat(head);
+    console.log(swapped)
+}
+
+console.log(swapHeadAndTail([ 1, 2, 3, 4, 5 ]), [ 4, 5, 3, 1, 2 ]);
+console.log(swapHeadAndTail([ -1, 2 ]), [ 2, -1 ]);
+console.log(swapHeadAndTail([ 1, 2, -3, 4, 5, 6, -7, 8 ]), [ 5, 6, -7, 8, 1, 2, -3, 4 ]);
